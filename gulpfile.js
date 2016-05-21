6afc8730bd6517f412125837d37559958822141a
Tate Carson <tatecarson@gmail.com>
2016-05-20 23:03:28 -0700

    lots of updates

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,9 @@ gulp.task('pug', function(){
  */
 gulp.task('watch', function () {
     gulp.watch('assets/css/**', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch('assets/js/**', ['jekyll-rebuild']);
+    gulp.watch(['index.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
+    gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
     gulp.watch(['_pugfiles/*.pug'], ['pug']);
 });
 
