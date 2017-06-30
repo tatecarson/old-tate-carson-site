d18de4bc841291dadd426022c577e914f70d06b5
tatecarson <tatecarson@gmail.com>
2017-06-29 23:23:30 -0700

    major updates

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -6,7 +6,6 @@ $(function() {
   mobileNav();
 });
 
-
 function mobileNav() {
   $('.mobile-nav-toggle').on('click', function(){
     var status = $(this).hasClass('is-open');
--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,69 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync');
-var sass        = require('gulp-sass');
-var prefix      = require('gulp-autoprefixer');
-var cp          = require('child_process');
-var pug         = require('gulp-pug');
-var ghPages     = require('gulp-gh-pages');
+var sass = require('gulp-sass');
+var prefix = require('gulp-autoprefixer');
+var cp = require('child_process');
+var pug = require('gulp-pug');
+var ghPages = require('gulp-gh-pages');
 
-var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var jekyll = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
 var messages = {
-    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
 };
 
 /**
  * Build the Jekyll Site
  */
-gulp.task('jekyll-build', function (done) {
-    browserSync.notify(messages.jekyllBuild);
-    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
-        .on('close', done);
+gulp.task('jekyll-build', function(done) {
+  browserSync.notify(messages.jekyllBuild);
+  return cp.spawn(jekyll, ['build'], {
+      stdio: 'inherit'
+    })
+    .on('close', done);
 });
 
 /**
  * Rebuild Jekyll & do page reload
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
-    browserSync.reload();
+gulp.task('jekyll-rebuild', ['jekyll-build'], function() {
+  browserSync.reload();
 });
 
 /**
  * Wait for jekyll-build, then launch the Server
  */
 gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
-    browserSync({
-        server: {
-            baseDir: '_site'
-        }
-    });
+  browserSync({
+    server: {
+      baseDir: '_site'
+    }
+  });
 });
 
 /**
  * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
  */
-gulp.task('sass', function () {
-    return gulp.src('assets/css/main.scss')
-        .pipe(sass({
-            includePaths: ['css'],
-            onError: browserSync.notify
-        }))
-        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
-        .pipe(browserSync.reload({stream:true}))
-        .pipe(gulp.dest('assets/css'));
+gulp.task('sass', function() {
+  return gulp.src('assets/css/main.scss')
+    .pipe(sass({
+      includePaths: ['css'],
+      onError: browserSync.notify
+    }))
+    .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest('_site/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+    .pipe(gulp.dest('assets/css'));
 });
 
 /*
 Gulp stuff
 */
-gulp.task('pug', function(){
-    return gulp.src('_pugfiles/*.pug')
+gulp.task('pug', function() {
+  return gulp.src('_pugfiles/*.pug')
     .pipe(pug())
     .pipe(gulp.dest('_includes'));
 })
@@ -67,12 +73,12 @@ gulp.task('pug', function(){
  * Watch scss files for changes & recompile
  * Watch html/md files, run jekyll & reload BrowserSync
  */
-gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
-    gulp.watch('assets/js/**', ['jekyll-rebuild']);
-    gulp.watch(['index.html', '_layouts/*.html', '_includes/*', 'CNAME'], ['jekyll-rebuild']);
-    gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
-    gulp.watch(['_pugfiles/*.pug'], ['pug']);
+gulp.task('watch', function() {
+  gulp.watch('assets/css/**', ['sass']);
+  gulp.watch('assets/js/**', ['jekyll-rebuild']);
+  gulp.watch(['index.html', '_layouts/*.html', '_includes/*', 'CNAME'], ['jekyll-rebuild']);
+  gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
+  gulp.watch(['_pugfiles/*.pug'], ['pug']);
 });
 
 /**
